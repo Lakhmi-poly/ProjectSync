7d8f256e58aa9e8fea4066ec7c1fc38b299caaac
yessmineLK <yessmine.lakhmi@polytechnicien.tn>
2025-04-29 01:56:50 +0200

    modification

--- gestionprojets/urls.py
+++ gestionprojets/urls.py
@@ -20,8 +20,8 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
+    path('projets/', include('projets.urls')),
     path('admin/', admin.site.urls),
-    path('', include('gestionprojets.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
--- projets/views.py
+++ projets/views.py
@@ -2,6 +2,24 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Projet, Tache
 from .forms import ProjetForm, TacheForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)  # Authentifie l'utilisateur
+            return redirect('accueil')  # Redirige vers une page d'accueil
+        else:
+            # Identifiants incorrects
+            return render(request, 'login.html', {'error': 'Identifiants invalides.'})
+    return render(request, 'login.html')
+
+
 
 @login_required
 def accueil(request):
